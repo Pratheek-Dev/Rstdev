af9e5b783f80ce463c5ba854a98a93a351f22a3a
Pratheek Devaraj <pratheekdevaraj@gmail.com>
2016-12-18 13:30:52 -0800

    added another test

--- tests/testthat/test_Rstdev.R
+++ tests/testthat/test_Rstdev.R
@@ -10,18 +10,7 @@ test_that("calcualtes standard deviation of list or vector", {
   expect_equal(standard_deviation(c(1,1,1)),0)
   expect_equal(standard_deviation(c(-5,-5)),0)
   expect_equal(standard_deviation(c()),NaN)
-  #expect_equal(standard_deviation("hello"),"invalid 'type' (character) of argument")
-
-
-})
-
-test_that("calcualtes standard error of list or vector", {
-
-  #
-  expect_equal(standard_error(1),0)
-  expect_equal(standard_error(c(1,1,1)),0)
-  expect_equal(standard_error(c(-5,-5)),0)
-  expect_equal(standard_error(c()),NaN)
+  expect_error(standard_deviation('something silly'),"invalid", ignore.case=TRUE)
 
 
 })
